e073932684e5e575eee2459cdb852b440e7bbecd
ZenRuin <bkimangelo@gmail.com>
2024-07-29 00:49:18 +0800

    Signed-off-by: ZenRuin <bkimangelo@gmail.com>

--- controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HomeController extends Controller {
-    public function handleRequest() {
-        $this->loadPage('home_landing', 'home');
-    }
-}
-
-?>
--- controllers/UsersController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class UsersController extends Controller {
-    public function handleRequest() {
-        $this->loadPage('users', 'users');
-    }
-}
-
-?>
